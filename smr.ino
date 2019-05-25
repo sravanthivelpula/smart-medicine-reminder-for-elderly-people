745d7eefea112b6e1db4dbeb5326f608c6974f48
sravanthivelpula <50827821+sravanthivelpula@users.noreply.github.com>
2019-05-25 13:10:11 +0530

    add filels to change

---
+++ smr.ino
@@ -0,0 +1,106 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+const char* ssid = "swathiga";
+const char* password = "ganeshswathi";
+
+#define ORG "ybg17p"
+#define DEVICE_TYPE "nodemcu"
+#define DEVICE_ID "swathi"
+#define TOKEN "7075606886"
+#define SSD1306_LCDHEIGHT 64
+
+// OLED display TWI address
+#define OLED_ADDR   0x3C
+
+Adafruit_SSD1306 display(-1);
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+String command;
+char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
+char topic[] = "iot-2/cmd/home/fmt/String";
+char authMethod[] = "use-token-auth";
+char token[] = TOKEN;
+char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
+//Serial.println(clientID);
+
+WiFiClient wifiClient;
+void callback(char* topic, byte* payload, unsigned int payloadLength);
+PubSubClient client(server, 1883, callback, wifiClient);
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  pinMode(D1,OUTPUT);
+  wifiConnect();
+  mqttConnect();
+    // initialize and clear display
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+// display a line of text
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.print("please take medicine");
+  // update display with all of the above graphics
+  display.display();
+}
+
+void loop() {
+  if (!client.loop()) {
+    mqttConnect();
+  }
+delay(100);
+}
+
+void wifiConnect() {
+  Serial.print("Connecting to "); Serial.print(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("nWiFi connected, IP address: "); Serial.println(WiFi.localIP());
+}
+
+void mqttConnect() {
+  if (!client.connected()) {
+    Serial.print("Reconnecting MQTT client to "); Serial.println(server);
+    while (!client.connect(clientId, authMethod, token)) {
+      Serial.print(".");
+      delay(500);
+    }
+    initManagedDevice();
+    Serial.println();
+  }
+}
+void initManagedDevice() {
+  if (client.subscribe(topic)) {
+    Serial.println("subscribe to cmd OK");
+  } else {
+    Serial.println("subscribe to cmd FAILED");
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int payloadLength) {
+  Serial.print("callback invoked for topic: "); Serial.println(topic);
+
+  for (int i = 0; i < payloadLength; i++) {
+    //Serial.println((char)payload[i]);
+    command += (char)payload[i];
+  }
+Serial.println(command);
+if(command == "ON"){
+  digitalWrite(D1,LOW);
+  Serial.println("Light is Switched ON");
+}
+else if(command == "OFF"){
+  digitalWrite(D1,HIGH);
+  Serial.println("Light is Switched OFF");
+}
+command ="";
+}
